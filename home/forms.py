972a295c267a8bf07e3e7ea3e974cc813c334a81
Manuel <elustondo129@gmail.com>
2020-10-09 16:17:56 -0500

    eliminar la migracion 0001

--- home/forms.py
+++ home/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from .models import *
 
 class citaform(forms.Form):
-    name =  forms.CharField(label="Nombre")
-    # tipo = 
-    # raza = 
-    # años = 
-    dueño = forms.CharField(label="Dueño")
-    consulta = forms.CharField(label="Consulta")
-    # fecha = 
-    # hora = 
+    mascota =  forms.CharField(label="Nombre", required=True)
+    tipo = forms.CharField(label="tipo", required=True)
+    raza = forms.CharField(label="raza", required=True)
+    años = forms.CharField(label="años", required=True)
+    dueño = forms.CharField(label="Dueño", required=True)
+    descripcion = forms.CharField(label="descripcion", required=True)
+    fecha = forms.DateField(label="fecha", required=True)
+    hora = forms.TimeField(label="hora", required=True)
--- home/models.py
+++ home/models.py
@@ -1,32 +1,3 @@
 from django.db import models
 
 # Create your models here.
-# Tabla Tipo de mascota
-class TipoMascota(models.Model):
-    mascota = models.CharField(max_length=255)
-    estado = models.IntegerField()
-
-    def __str__(self):
-        return self.mascota
-
-# Tabla de la raza de la mascota
-class Raza(models.Model):
-    raza = models.CharField(max_length=255)
-    estado = models.IntegerField()
-    id_tipo = models.ForeignKey(TipoMascota, on_delete=models.PROTECT)
-    
-    def __str__(self):
-        return self.raza
-
-# Tabla de la cita
-class Citas(models.Model):
-    mascota = models.CharField(max_length=255)
-    edad = models.IntegerField()
-    fecha = models.DateField()
-    hora = models.TimeField()
-    dueño = models.CharField(max_length=255)
-    estado = models.IntegerField()
-    raza = models.ForeignKey(Raza, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.mascota + ' ' +str(self.id)
